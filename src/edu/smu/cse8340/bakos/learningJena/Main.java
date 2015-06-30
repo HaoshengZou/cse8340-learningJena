7dac61c57e67155964ffa5e42eb5a0fd3657264b
Yong Bakos <ybakos@humanoriented.com>
2015-06-29 18:19:35 -0600

    Initial commit.

---
+++ src/edu/smu/cse8340/bakos/learningJena/Main.java
@@ -0,0 +1,17 @@
+/*
+  Main.java
+  This is a simple wrapper for bootstrapping the program.
+  @author Yong Joseph Bakos
+    
+  CSE 8340 Assignment 4: Modeling and querying with Apache Jena and SPARQL.
+  TODO: Description
+*/
+package edu.smu.cse8340.bakos.learningJena;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("TODO");
+    }
+
+}
