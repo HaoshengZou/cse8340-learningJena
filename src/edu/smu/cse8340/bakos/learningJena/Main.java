ead8222b6962d105e979436ef2b2e5c5a1334299
Yong Bakos <ybakos@humanoriented.com>
2015-07-06 22:12:27 -0600

    Instantiate OWL inference model with ontology and data.

--- src/edu/smu/cse8340/bakos/learningJena/Main.java
+++ src/edu/smu/cse8340/bakos/learningJena/Main.java
@@ -8,10 +8,27 @@
 */
 package edu.smu.cse8340.bakos.learningJena;
 
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.*; 
+import com.hp.hpl.jena.reasoner.*; 
+import com.hp.hpl.jena.util.*;
+
 public class Main {
 
+    private final static String ONTOLOGY_URL = "http://lyle.smu.edu/~ybakos/cse8340/a4/foafonto.ttl";
+    private final static String DATA_URL = "http://lyle.smu.edu/~ybakos/cse8340/a4/foafdata.ttl";
+    private final static String RDF_SYNTAX = "N3";
+
     public static void main(String[] args) {
-        System.out.println("TODO");
+        Model dataModel = ModelFactory.createDefaultModel();
+        dataModel.read(DATA_URL, RDF_SYNTAX);
+        Model schema = ModelFactory.createDefaultModel();
+        schema.read(ONTOLOGY_URL, RDF_SYNTAX);
+
+        Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+        reasoner = reasoner.bindSchema(schema);
+        InfModel inferenceModel = ModelFactory.createInfModel(reasoner, dataModel);
+        inferenceModel.write(System.out, RDF_SYNTAX);
     }
 
 }
