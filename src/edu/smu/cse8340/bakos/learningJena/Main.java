159608f3d8c92c1a7a0b017bc990e2235949ed40
Yong Bakos <ybakos@humanoriented.com>
2015-07-07 17:03:42 -0600

    Add movie query and JSON output. Part B.

--- src/edu/smu/cse8340/bakos/learningJena/Main.java
+++ src/edu/smu/cse8340/bakos/learningJena/Main.java
@@ -115,26 +115,45 @@ public class Main {
         "}";
 
 
-    public static void main(String[] args) {
-        Model dataModel = ModelFactory.createDefaultModel();
-        dataModel.read(DATA_URL, RDF_SYNTAX);
-        for (String url : CLASS_MEMBER_URLS) {
-            try {
-                dataModel.read(url, RDF_SYNTAX);
-            } catch (HttpException e) {
-                System.out.println("Problem with: " + url);
-            } catch (RiotException e) {
-                System.out.println("Problem with: " + url);
-            } catch (RuntimeIOException e) {
-                System.out.println("Problem with: " + url);
-            }
-        }
-        Model schema = ModelFactory.createDefaultModel();
-        schema.read(ONTOLOGY_URL, RDF_SYNTAX);
+    public static final String MOVIE_QUERY = 
+        "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+        "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+        "PREFIX oddlinker: <http://data.linkedmdb.org/resource/oddlinker/>\n" +
+        "PREFIX map: <file:/C:/d2r-server-0.4/mapping.n3#>\n" +
+        "PREFIX db: <http://data.linkedmdb.org/resource/>\n" +
+        "PREFIX dbpedia: <http://dbpedia.org/property/>\n" +
+        "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+        "PREFIX dc: <http://purl.org/dc/terms/>\n" +
+        "PREFIX movie: <http://data.linkedmdb.org/resource/movie/>\n" +
+        "SELECT ?name\n" +
+        "WHERE {\n" +
+        "<http://data.linkedmdb.org/resource/actor/29364> <http://data.linkedmdb.org/resource/movie/performance> ?movie .\n" +
+        "?movie <http://data.linkedmdb.org/resource/movie/performance_film> ?name\n" +
+        "}";
 
-        Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-        reasoner = reasoner.bindSchema(schema);
-        InfModel inferenceModel = ModelFactory.createInfModel(reasoner, dataModel);
+    public static void main(String[] args) {
+        // Model dataModel = ModelFactory.createDefaultModel();
+        // dataModel.read(DATA_URL, RDF_SYNTAX);
+        // for (String url : CLASS_MEMBER_URLS) {
+        //     try {
+        //         dataModel.read(url, RDF_SYNTAX);
+        //     } catch (HttpException e) {
+        //         System.out.println("Problem with: " + url);
+        //     } catch (RiotException e) {
+        //         System.out.println("Problem with: " + url);
+        //     } catch (RuntimeIOException e) {
+        //         System.out.println("Problem with: " + url);
+        //     }
+        // }
+        // Model schema = ModelFactory.createDefaultModel();
+        // schema.read(ONTOLOGY_URL, RDF_SYNTAX);
+
+        // Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+        // reasoner = reasoner.bindSchema(schema);
+        // InfModel inferenceModel = ModelFactory.createInfModel(reasoner, dataModel);
         //inferenceModel.write(System.out, RDF_SYNTAX);
         // System.out.println("6. OWL Inferencing of foaf:knows symmetry.\n");
         // System.out.println(SYMMETRY_QUERY);
@@ -163,15 +182,19 @@ public class Main {
         // System.out.println("Result:");
         // m.write(System.out, RDF_SYNTAX);
 
-        System.out.println("9. DESCRIBING all the owl:sameAs triples across multiple resource endpoints.\n");
-        System.out.println(INTEGRATION_QUERY);
-        Query query = QueryFactory.create(INTEGRATION_QUERY);
-        QueryExecution qe = QueryExecutionFactory.create(query, inferenceModel);
-        Model m = qe.execDescribe();
-        System.out.println("Result:");
-        m.write(System.out, RDF_SYNTAX);
-
-
+        // System.out.println("9. DESCRIBING all the owl:sameAs triples across multiple resource endpoints.\n");
+        // System.out.println(INTEGRATION_QUERY);
+        // Query query = QueryFactory.create(INTEGRATION_QUERY);
+        // QueryExecution qe = QueryExecutionFactory.create(query, inferenceModel);
+        // Model m = qe.execDescribe();
+        // System.out.println("Result:");
+        // m.write(System.out, RDF_SYNTAX);
+        
+        ResultSet rs = QueryExecutionFactory.sparqlService( "http://data.linkedmdb.org/sparql", MOVIE_QUERY).execSelect();
+        ResultSetFormatter.outputAsJSON(System.out, rs);
+        // while (rs.hasNext()) {
+        //     System.out.println(rs.next());
+        // }
     }
 
 }
